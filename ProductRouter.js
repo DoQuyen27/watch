2fd23fc0b6ce6c8d738c8f67bb4c6faf562daa0a
DoQuyen27 <doquyen271708@gmail.com>
2023-11-29 18:59:41 +0700

    Thêm các tệp tin mới

---
+++ ProductRouter.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router()
+const ProductCtrl = require("../../src/controllers/ProductCtrl")
+
+router.post('/product',ProductCtrl.createProduct)
+router.delete('/product/:id',ProductCtrl.deleteProduct)
+router.put('/product/:id', ProductCtrl.updateProduct);
+module.exports = router
